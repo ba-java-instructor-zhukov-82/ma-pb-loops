627fd01f66a79bde2da922aa5820a695dfcd44b4
ba-java-instructor-zhukov-82 <ba.java.instructor.zhukov.82@gmail.com>
2017-02-10 20:28:04 +0200

    For each added

--- scripts/script.js
+++ scripts/script.js
@@ -17,11 +17,57 @@ function whileLoopExample() {
 
     var numberCopy = number;
     var digits = 0;
-    while(numberCopy > 0) {
+    while (numberCopy > 0) {
         numberCopy = Math.floor(numberCopy / 10);
         ++digits;
     }
 
     var output = 'Number ' + number + ' has ' + digits + ' digits!';
     print(output);
+}
+
+function doWhileLoopExample() {
+
+    var iteration = 1;
+
+    do {
+        alert('Doing some program functionality. Iteration ' + iteration);
+
+        var continueExecution = confirm('Do you want to continue?');
+        if (!continueExecution) {
+            break;
+        }
+
+        ++iteration;
+    } while (true);
+}
+
+function forLoopExample() {
+
+    var result = '';
+    for (var i = 100; i >= 1; --i) {
+        var separator = i == 1 ? '' : ', ';
+        result += i + separator;
+        if ((i - 1) % 10 == 0)
+            result += '<br/>';
+    }
+
+    print(result);
+}
+
+function forEachLoopExample() {
+              // 0  1  2  3  4  5  6  7  8  9
+    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    var result = '';
+    for (var index in array) {
+        var separator = index == array.length - 1 ? '' : ', ';
+        result += array[index] + separator;
+        if ((index + 1) % 5 == 0) {
+            alert('test');
+            result += '<br/>';
+        }
+    }
+
+    print(result);
 }
